83a46ce3b74b42894a1afe00a43a8cdabe81b735
MellySusanti <mellyfang7713@gmail.com>
2021-11-21 19:00:24 +0700

    Pertemuan 6 - Router Render

--- melly_susanti/src/App.js
+++ melly_susanti/src/App.js
@@ -1,129 +1,33 @@
-import React, { Component } from 'react';
-import './css/bootstrap.min.css';
+import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from './views/Home/Home.js';
-import About from './views/About/About';
-import Main from './Main'
-import Header from './Header'
-import Help from './views/Help/Help'
+import {BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
 
-// class App extends Component {
-// constructor(){
-//   super();
-//   //inisial state view
-//   this.state = {
-//     view : 'home'
-//   }
-// }
-
-//   render(){
-//     //Functional component view untuk mengatur component yang tampil
-//     const View = ()=>{
-//       if(this.state.view == 'home')
-//         return <Main />
-//       else if(this.state.view == 'about')
-//         return <About />
-//       else if(this.state.view == 'help')
-//         return <Help />
-//     }
-
-//     return(
-//       <div>
-//         <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-//           <ul className="navbar-nav">
-//             <li className="nav-item">
-//               <a onClick={()=> this.setState({view : 'home'})} className="nav-link" href="#">
-//                 Home
-//               </a>
-//             </li>
-
-//             <li className="nav-item">
-//               <a onClick={()=> this.setState({view : 'about'})} className="nav-link" href="#">
-//                 About
-//               </a>
-//             </li>
-
-//             <li className="nav-item">
-//               <a onClick={()=> this.setState({view : 'help'})} className="nav-link" href="#">
-//                 Help
-//               </a>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         <View /> {/* panggil component View */}
-//       </div>
-//     );
-//   }
-// }
-
-//Props
-function Message(props){
-  return <p>Hello {props.name},</p>;
-}
-
-class App extends Component{
-  render(){
-    return(
-      <div>
-        <Message name="Ulrich" />
-        <Home name="Eden Hazard" />
-      </div>
-    );
-  }
-}
-
-//Class Component
-// function Message(){
-//   return "Pesan didalam Component";
-// }
-
-// class App extends Component{
-//   render(){
-//     return(
-//       <div>
-//         <Message />
-//         <Home />
-//       </div>
-//     );
-//   }
-// }
-
-//FUNCTIONAL COMPONENTS
-// function Message(){
-//   return <p>Hello from Message component</p>
-// }
-
-// class App extends Component{
-//   render(){
-//     return(
-//       <div>
-//         <Message />
-//       </div>
-//     );
-//   }
-// }
-
-//FUNCTION
-// function sayHello(){
-//   return "Hello World"
-// }
-
-// class App extends Component{
+// render Router
+class App extends Component {
+    render(){
+      return(
+        <Router>
+          <div>
+            <ul style={{listStyle: 'none'}}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/news">News</Link></li>
+            </ul>
   
-//   bilangHalo(){
-//     return "Halo Dunia"
-//   }
-
-//   render(){
-//     return(
-//       <>
-//         <p>sayHello : <b>{sayHello()}</b></p>
-//         <p>bilangHalo : <b>{this.bilangHalo()}</b></p>
-//       </>
-//     )
-//   }
-// }
-
+            <Switch>
+              <Route path='/' exact render={() => <div> Ini halaman home</div>} />
+              <Route path='/news' render={() => <div>Ini halaman News</div>}/>          </Switch>
+  
+          </div>
+        </Router>
+      );
+    }
+  }
 
 export default App;
